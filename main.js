193bb174c69c33e9f6a641f33be6cf2662eb6a12
Dashanesta <71793564+Dashanesta@users.noreply.github.com>
2024-03-15 23:37:50 -0400

    In process of making album search work. Non-functional buttons appear for first 10 results

--- main.js
+++ main.js
@@ -1,14 +1,43 @@
-async function search() {
+var searchMode = 'Album';
+const searchBox = document.getElementById('SearchBox');
+const MBIDButton = document.getElementById('MBIDSelector');
+const AlbumButton = document.getElementById('AlbumSelector');
+const ArtistButton = document.getElementById('ArtistSelector');
+
+async function Search() {
+    var query = document.getElementById("SearchBox").value;
     document.getElementById("albumtracklist").innerHTML = "";
-    var mbid = document.getElementById("MBIDInput").value;
-    //var tracklist = getAlbum(mbid);
-    var tracklist = await getAlbum(mbid);
-    arrayToList(tracklist, "albumtracklist");
+    document.getElementById("albumresults").innerHTML = "";
+    getAlbum(query);
+    }
+
+async function getAlbum(query) {
+    switch (searchMode) {
+        case 'MBID':
+            ShowTracklist(await LoadTracklist(query), "albumtracklist");
+            break;
+        case 'Album':
+            let x = await fetch(`https://musicbrainz.org/ws/2/release?query=${query}&limit=10&inc=recordings&fmt=json`)
+            let result = await x.json();
+            let albums = result.releases;
+            let formattedResults = [];
+            for(let i = 0; i < albums.length; i++) {
+                //TO DO add for loop to check for multiple artists
+                let artist = albums[i]["artist-credit"][0].name;
+                let album = albums[i].title;
+                formattedResults[i] = [artist, album];
+            }
+            console.log(formattedResults);
+            ShowAlbumSearchResults(formattedResults, "albumresults");
+            break;
+        case 'Artist':
+            break;
+    }
 }
 
-async function getAlbum(mbid) {
+async function LoadTracklist(MBID) {
     try {
-        let x = await fetch(`https://musicbrainz.org/ws/2/release/${mbid}?inc=recordings&fmt=json`);
+        let x = await fetch(`https://musicbrainz.org/ws/2/release/${MBID}?inc=recordings&fmt=json`);
         let album = await x.json();
         let tracks = album.media[0].tracks;
         let tracklist = [];
@@ -23,7 +52,19 @@ async function getAlbum(mbid) {
     }
 }
 
-function arrayToList(array, listId) {
+function ShowAlbumSearchResults(array, listId) {
+    let list = document.getElementById(listId);
+        for (i = 0; i < array.length; ++i) {
+            let button = document.createElement('button');
+            let li = document.createElement('li');
+            li.innerText = array[i];
+            button.setAttribute("id", "albumresult");
+            list.appendChild(button);
+            button.appendChild(li);
+        }
+}
+
+function ShowTracklist(array, listId) {
     let list = document.getElementById(listId);
         for (i = 0; i < array.length; ++i) {
             let li = document.createElement('li');
@@ -32,4 +73,35 @@ function arrayToList(array, listId) {
             li.setAttribute("id", "albumtrack");
             list.appendChild(li);
         }
+}
+
+function SearchMode(mode) {
+    switch (mode) {
+        case 'MBID':
+            AlbumButton.setAttribute("class", "btn btn-outline-primary");
+            ArtistButton.setAttribute("class", "btn btn-outline-primary");
+            MBIDButton.setAttribute("class", "btn btn-primary");
+            searchMode != 'MBID' ? searchBox.value = '' : null;
+            searchBox.setAttribute("placeholder", "Enter MBID:");
+            searchMode = 'MBID';
+            break;
+
+        case 'Album':
+            AlbumButton.setAttribute("class", "btn btn-primary");
+            ArtistButton.setAttribute("class", "btn btn-outline-primary");
+            MBIDButton.setAttribute("class", "btn btn-outline-primary");
+            searchMode != 'Album' ? searchBox.value = '' : null;
+            searchBox.setAttribute("placeholder", "Enter Album:");
+            searchMode = 'Album';
+            break;
+
+        case 'Artist':
+            AlbumButton.setAttribute("class", "btn btn-outline-primary");
+            ArtistButton.setAttribute("class", "btn btn-primary");
+            MBIDButton.setAttribute("class", "btn btn-outline-primary");
+            searchMode != 'Artist' ? searchBox.value = '' : null;
+            searchBox.setAttribute("placeholder", "Enter Artist:");
+            searchMode = 'Artist';
+            break;
+    }
 }
